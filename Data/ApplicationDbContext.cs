80ac21943f69f6bb30eb36e4d63510d642a275e5
RishiChaturvedi-7 <rishichaturvedi700@gmail.com>
2024-12-16 18:14:46 +0530

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿namespace BulkyWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options)
+    {
+        
+    }
+}
